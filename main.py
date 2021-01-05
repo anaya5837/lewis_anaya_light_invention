d60aa811777bfeb68ae0f1ff3e654618db0b9935
anaya5837 <72808504+anaya5837@users.noreply.github.com>
2021-01-05 10:34:58 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 while True:
     print("Light Level: " + input.light_level())
-    if input.light_level() >= 16 : 
-        light.clear
-    elif input.light_level() <= 15:
+   if input.light_level() <= 20:
         light.set_all(light.rgb(250, 250, 250))
         light.set_brightness(5)
     else:
