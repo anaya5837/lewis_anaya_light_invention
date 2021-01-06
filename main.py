4b29dbca2c6b8a902cabfb398f34190a6fe9c8e4
anaya5837 <72808504+anaya5837@users.noreply.github.com>
2021-01-05 20:45:28 -0500

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 while True:
     print("Light Level: " + input.light_level())
-    if input.light_level() <= 20:
+    if input.light_level() <= 24:
         light.set_all(light.rgb(250, 250, 250))
         light.set_brightness(5)
     else:
