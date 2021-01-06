8e9fad51361a5ce63bf31b843f8742b7d80daf04
anaya5837 <72808504+anaya5837@users.noreply.github.com>
2021-01-06 08:20:50 -0500

    Update main.ts, main.py

--- main.ts
+++ main.ts
@@ -1,6 +1,6 @@
 while (true) {
     console.log("Light Level: " + input.lightLevel())
-    if (input.lightLevel() <= 5) {
+    if (input.lightLevel() < 3) {
         light.setAll(light.rgb(250, 250, 250))
         light.setBrightness(5)
     } else {
